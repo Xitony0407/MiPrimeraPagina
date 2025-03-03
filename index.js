190aeefe06caa38d33903335871a864a22596908
Xitony0407 <gutierrezperezximena4@gmail.com>
2025-03-02 19:36:01 -0600

    Trabajando el footer

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ const develop2 = "Sigo Desarrollando cositas 2";
 const feature = "Haciendo feature";
 
 const feature2 = "Haciendo feature 2";
+
+<div>
+  <p>Copyright 2025</p>
+  <footer>Trabajando en el footer</footer>
+</div>
